87f2f6df6cf372a7f98deae41ec86ddff6a3c943
ADV\jason-jd.chen <jason-jd.chen@advantest.com>
2024-08-26 15:12:15 +0800

    create CMS route & navigation

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,21 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '@/views/MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // },
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: 'main',
+      component: MainView,
+      children: [
+        { path: '/categories/create', component: () => import('@/views/category/CategoryEdit.vue') },
+        { path: '/categories/edit/:id', component: () => import('@/views/category/CategoryEdit.vue'), props: true },
+        { path: '/categories/list', component: () => import('@/views/category/CategoryList.vue') },
+
+        { path: '/ingredients/create', component: () => import('@/views/ingredient/IngredientEdit.vue') },
+        { path: '/ingredients/edit/:id', component: () => import('@/views/ingredient/IngredientEdit.vue'), props: true },
+        { path: '/ingredients/list', component: () => import('@/views/ingredient/IngredientList.vue') },
+
+        { path: '/drink/create', component: () => import('@/views/drink/DrinkEdit.vue') },
+        { path: '/drink/edit/:id', component: () => import('@/views/drink/DrinkEdit.vue'), props: true },
+        { path: '/drink/list', component: () => import('@/views/drink/DrinkList.vue') },
+
+        { path: '/articles/create', component: () => import('@/views/article/ArticleEdit.vue') },
+        { path: '/articles/edit/:id', component: () => import('@/views/article/ArticleEdit.vue'), props: true },
+        { path: '/articles/list', component: () => import('@/views/article/ArticleList.vue') },
+
+        { path: '/stores/create', component: () => import('@/views/store/StoreEdit.vue') },
+        { path: '/stores/edit/:id', component: () => import('@/views/store/StoreEdit.vue'), props: true },
+        { path: '/stores/list', component: () => import('@/views/store/StoreList.vue') },
+
+        { path: '/users/create', component: () => import('@/views/user/UserEdit.vue') },
+        { path: '/users/edit/:id', component: () => import('@/views/user/UserEdit.vue'), props: true },
+        { path: '/users/list', component: () => import('@/views/user/UserList.vue') },
+      ]
     }
   ]
 })
---
+++ admin/src/views/MainView.vue
@@ -0,0 +1,82 @@
+<script setup>
+</script>
+
+<template>
+  <el-container style="height: 100vh">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-menu router unique-opened :default-active="$route.path">
+        <el-sub-menu index="1">
+          <template #title>
+            <i class="el-icon-message"></i>網站內容管理
+          </template>
+          <el-menu-item-group>
+            <template #title>配料</template>
+            <el-menu-item index="/ingredients/create">新建配料</el-menu-item>
+            <el-menu-item index="/ingredients/list">配料列表</el-menu-item>
+          </el-menu-item-group>
+
+          <el-menu-item-group>
+            <template #title>飲料品項</template>
+            <el-menu-item index="/drink/create">新建飲料</el-menu-item>
+            <el-menu-item index="/drink/list">飲料列表</el-menu-item>
+          </el-menu-item-group>
+
+          <el-menu-item-group>
+            <template #title>文章</template>
+            <el-menu-item index="/articles/create">新建文章</el-menu-item>
+            <el-menu-item index="/articles/list">文章列表</el-menu-item>
+          </el-menu-item-group>
+        </el-sub-menu>
+
+        <el-sub-menu index="2">
+          <template #title> <i class="el-icon-message"></i>行銷管理 </template>
+          <el-menu-item-group>
+            <template #title>門市</template>
+            <el-menu-item index="/stores/create">新建門市</el-menu-item>
+            <el-menu-item index="/stores/list">門市列表</el-menu-item>
+          </el-menu-item-group>
+        </el-sub-menu>
+
+        <el-sub-menu index="3">
+          <template #title> <i class="el-icon-message"></i>系統設置 </template>
+          <el-menu-item-group>
+            <template #title>分類</template>
+            <el-menu-item index="/categories/create">新建分類</el-menu-item>
+            <el-menu-item index="/categories/list">分類列表</el-menu-item>
+          </el-menu-item-group>
+
+          <el-menu-item-group>
+            <template #title>管理員</template>
+            <el-menu-item index="/users/create">新建管理員</el-menu-item>
+            <el-menu-item index="/users/list">管理員列表</el-menu-item>
+          </el-menu-item-group>
+
+          <!-- <el-menu-item-group>
+            <template #title>系統</template>
+            <el-menu-item index="/login">登出</el-menu-item>
+          </el-menu-item-group> -->
+        </el-sub-menu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px"></el-header>
+
+      <el-main>
+        <RouterView :key="$route.path"></RouterView>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
