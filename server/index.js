64d7ebc31692c6c94641bda0122abae386abda7d
ADV\jason-jd.chen <a0984009426@gmail.com>
2025-03-21 00:50:37 +0800

    update server/index.js to fix api not found in vercel

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok", time: new Date().toISOString() });
 });
-app.use("/api", apiRoutes);
+app.use("/", apiRoutes);
 app.use(handleError);
 
 const PORT = process.env.PORT || 3000;
