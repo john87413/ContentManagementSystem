d626601cf40a47c0f6290c25aa91bed70bc80abe
ADV\jason-jd.chen <a0984009426@gmail.com>
2025-03-22 10:58:03 +0800

    import element-plus component per requirement

--- admin/src/components/GenericList.vue
+++ admin/src/components/GenericList.vue
@@ -49,7 +49,7 @@
 </template>
 
 <script setup>
-import { ref, reactive, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { ElMessage, ElMessageBox } from "element-plus";
 
--- admin/src/main.js
+++ admin/src/main.js
@@ -1,17 +1,89 @@
-import './assets/main.css'
-import 'element-plus/dist/index.css'
-
+// 引入需要的組件
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
+import {
+  ElButton,
+  ElInput,
+  ElForm,
+  ElFormItem,
+  ElTable,
+  ElTableColumn,
+  ElPagination,
+  ElSelect,
+  ElOption,
+  ElMenu,
+  ElMenuItem,
+  ElMenuItemGroup,
+  ElSubMenu,
+  ElContainer,
+  ElHeader,
+  ElAside,
+  ElMain,
+  ElTabs,
+  ElTabPane,
+  ElUpload,
+  ElIcon,
+  ElDialog,
+  ElInputNumber,
+  ElSwitch,
+  ElRadio,
+  ElRadioGroup,
+  ElRate,
+  ElLoading,
+  ElRow,
+  ElCol
+} from 'element-plus'
+
+// 引入樣式
+import './assets/main.css'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+// 註冊組件
+const components = [
+  ElButton,
+  ElInput,
+  ElForm,
+  ElFormItem,
+  ElTable,
+  ElTableColumn,
+  ElPagination,
+  ElSelect,
+  ElOption,
+  ElMenu,
+  ElMenuItem,
+  ElMenuItemGroup,
+  ElSubMenu,
+  ElContainer,
+  ElHeader,
+  ElAside,
+  ElMain,
+  ElTabs,
+  ElTabPane,
+  ElUpload,
+  ElIcon,
+  ElDialog,
+  ElInputNumber,
+  ElSwitch,
+  ElRadio,
+  ElRadioGroup,
+  ElRate,
+  ElRow,
+  ElCol
+]
+
+// 批量註冊組件
+components.forEach(component => {
+  app.component(component.name, component)
+})
+
+
+app.use(ElLoading)
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
 
 app.mount('#app')
