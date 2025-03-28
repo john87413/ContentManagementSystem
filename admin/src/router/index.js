e0b3032022f6237610a7017a176a3e09d7260163
ADV\jason-jd.chen <a0984009426@gmail.com>
2025-03-28 11:09:14 +0800

    directly import component when first time loading page

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -5,6 +5,22 @@ import MainView from '@/views/MainView.vue'
 import Login from '@/views/Login.vue'
 import Register from '@/views/Register.vue'
 
+// Prefetch components for better performance
+const CategoryEdit = () => import(/* webpackChunkName: "category" */ '@/views/category/CategoryEdit.vue')
+const CategoryList = () => import(/* webpackChunkName: "category" */ '@/views/category/CategoryList.vue')
+const IngredientEdit = () => import(/* webpackChunkName: "ingredient" */ '@/views/ingredient/IngredientEdit.vue')
+const IngredientList = () => import(/* webpackChunkName: "ingredient" */ '@/views/ingredient/IngredientList.vue')
+const DrinkEdit = () => import(/* webpackChunkName: "drink" */ '@/views/drink/DrinkEdit.vue')
+const DrinkList = () => import(/* webpackChunkName: "drink" */ '@/views/drink/DrinkList.vue')
+const ArticleEdit = () => import(/* webpackChunkName: "article" */ '@/views/article/ArticleEdit.vue')
+const ArticleList = () => import(/* webpackChunkName: "article" */ '@/views/article/ArticleList.vue')
+const CarouselEdit = () => import(/* webpackChunkName: "carousel" */ '@/views/carousel/CarouselEdit.vue')
+const CarouselList = () => import(/* webpackChunkName: "carousel" */ '@/views/carousel/CarouselList.vue')
+const ShopEdit = () => import(/* webpackChunkName: "shop" */ '@/views/shop/ShopEdit.vue')
+const ShopList = () => import(/* webpackChunkName: "shop" */ '@/views/shop/ShopList.vue')
+const UserEdit = () => import(/* webpackChunkName: "user" */ '@/views/user/UserEdit.vue')
+const UserList = () => import(/* webpackChunkName: "user" */ '@/views/user/UserList.vue')
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -25,33 +41,33 @@ const router = createRouter({
       name: 'main',
       component: MainView,
       children: [
-        { path: '/categories/create', component: () => import('@/views/category/CategoryEdit.vue'), meta: { requiredPermissions: ['contentManagement'] } },
-        { path: '/categories/edit/:id', component: () => import('@/views/category/CategoryEdit.vue'), props: true, meta: { requiredPermissions: ['contentManagement'] } },
-        { path: '/categories/list', component: () => import('@/views/category/CategoryList.vue'), meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/categories/create', component: CategoryEdit, meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/categories/edit/:id', component: CategoryEdit, props: true, meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/categories/list', component: CategoryList, meta: { requiredPermissions: ['contentManagement'] } },
 
-        { path: '/ingredients/create', component: () => import('@/views/ingredient/IngredientEdit.vue'), meta: { requiredPermissions: ['contentManagement'] } },
-        { path: '/ingredients/edit/:id', component: () => import('@/views/ingredient/IngredientEdit.vue'), props: true, meta: { requiredPermissions: ['contentManagement'] } },
-        { path: '/ingredients/list', component: () => import('@/views/ingredient/IngredientList.vue'), meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/ingredients/create', component: IngredientEdit, meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/ingredients/edit/:id', component: IngredientEdit, props: true, meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/ingredients/list', component: IngredientList, meta: { requiredPermissions: ['contentManagement'] } },
 
-        { path: '/drinks/create', component: () => import('@/views/drink/DrinkEdit.vue'), meta: { requiredPermissions: ['contentManagement'] } },
-        { path: '/drinks/edit/:id', component: () => import('@/views/drink/DrinkEdit.vue'), props: true, meta: { requiredPermissions: ['contentManagement'] } },
-        { path: '/drinks/list', component: () => import('@/views/drink/DrinkList.vue'), meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/drinks/create', component: DrinkEdit, meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/drinks/edit/:id', component: DrinkEdit, props: true, meta: { requiredPermissions: ['contentManagement'] } },
+        { path: '/drinks/list', component: DrinkList, meta: { requiredPermissions: ['contentManagement'] } },
 
-        { path: '/articles/create', component: () => import('@/views/article/ArticleEdit.vue'), meta: { requiredPermissions: ['marketingManagement'] } },
-        { path: '/articles/edit/:id', component: () => import('@/views/article/ArticleEdit.vue'), props: true, meta: { requiredPermissions: ['marketingManagement'] } },
-        { path: '/articles/list', component: () => import('@/views/article/ArticleList.vue'), meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/articles/create', component: ArticleEdit, meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/articles/edit/:id', component: ArticleEdit, props: true, meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/articles/list', component: ArticleList, meta: { requiredPermissions: ['marketingManagement'] } },
 
-        { path: '/carousels/create', component: () => import('@/views/carousel/CarouselEdit.vue'), meta: { requiredPermissions: ['marketingManagement'] } },
-        { path: '/carousels/edit/:id', component: () => import('@/views/carousel/CarouselEdit.vue'), props: true, meta: { requiredPermissions: ['marketingManagement'] } },
-        { path: '/carousels/list', component: () => import('@/views/carousel/CarouselList.vue'), meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/carousels/create', component: CarouselEdit, meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/carousels/edit/:id', component: CarouselEdit, props: true, meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/carousels/list', component: CarouselList, meta: { requiredPermissions: ['marketingManagement'] } },
 
-        { path: '/shops/create', component: () => import('@/views/shop/ShopEdit.vue'), meta: { requiredPermissions: ['marketingManagement'] } },
-        { path: '/shops/edit/:id', component: () => import('@/views/shop/ShopEdit.vue'), props: true, meta: { requiredPermissions: ['marketingManagement'] } },
-        { path: '/shops/list', component: () => import('@/views/shop/ShopList.vue'), meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/shops/create', component: ShopEdit, meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/shops/edit/:id', component: ShopEdit, props: true, meta: { requiredPermissions: ['marketingManagement'] } },
+        { path: '/shops/list', component: ShopList, meta: { requiredPermissions: ['marketingManagement'] } },
 
-        { path: '/users/create', component: () => import('@/views/user/UserEdit.vue'), meta: { requiredPermissions: ['systemSettings'] } },
-        { path: '/users/edit/:id', component: () => import('@/views/user/UserEdit.vue'), props: true, meta: { requiredPermissions: ['systemSettings'] } },
-        { path: '/users/list', component: () => import('@/views/user/UserList.vue'), meta: { requiredPermissions: ['systemSettings'] } },
+        { path: '/users/create', component: UserEdit, meta: { requiredPermissions: ['systemSettings'] } },
+        { path: '/users/edit/:id', component: UserEdit, props: true, meta: { requiredPermissions: ['systemSettings'] } },
+        { path: '/users/list', component: UserList, meta: { requiredPermissions: ['systemSettings'] } },
       ]
     }
   ]
